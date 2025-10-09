c8d268ee3f5fa2cdfd14bf2b9246c9963179f07f
abinfpro <abinkummil@gmail.com>
2025-10-09 20:51:31 +0530

    11th commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
+
